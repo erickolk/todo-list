74cc48a67a10b708d0923430f168f09e0d7f8973
Erickolk <erickdynsouza@hotmail.com>
2021-11-18 09:17:11 -0300

    Projeto to do let's go

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <img alt="JS logo" src="./assets/logojs.png" height="195" width="195">
-    <HelloWorld msg="Aprendendo a mexer nessa bagaça"/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -24,6 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
